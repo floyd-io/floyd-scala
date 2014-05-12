bb0e0169e25cca4896e3457a6286e7bcb6d3ae44
Cruz Fernandez <cruz@scvsoft.com>
2014-05-12 17:17:36 +0200

    Refactored to use an scalable approach of Actors so that it can be paralelized on multiple machines

--- src/main/scala/io/floyd/FloydServiceActor.scala
+++ src/main/scala/io/floyd/FloydServiceActor.scala
@@ -8,7 +8,42 @@ import spray.can.Http
 import HttpMethods._
 import MediaTypes._
 import scala.concurrent.duration._
+import scala.collection.mutable
 
+
+
+class StreamerActor extends Actor with ActorLogging {
+  import context.dispatcher // ExecutionContext for the futures and scheduler
+
+  // we use the successful sending of a chunk as trigger for scheduling the next chunk
+
+  def receive = {
+    log.debug("Starting streaming response ...")
+    case StartStream(client) =>
+      client ! ChunkedResponseStart(HttpResponse(entity = "")).withAck(Ok(5, client))
+
+    case Ok(0, client) =>
+      log.info("Finalizing response stream ...")
+      client ! MessageChunk("\nStopped...")
+      client ! ChunkedMessageEnd
+
+    case Ok(remaining, client) =>
+      log.info("Sending response chunk ...")
+      log.info(this.toString())
+      log.info(self.toString())
+      context.system.scheduler.scheduleOnce(1000 millis span) {
+        val json = s"""{
+            date:"${DateTime.now.toIsoDateTimeString.toString()}",
+            count:"$remaining"
+          }
+          """
+        client ! MessageChunk(json).withAck(Ok(remaining - 1, client))
+      }
+  }
+
+  // simple case class whose instances we use as send confirmation message for streaming chunks
+  case class Ok(remaining: Int, client: ActorRef)
+}
 class FloydServiceActor extends Actor {
   
   import context.dispatcher // ExecutionContext for the futures and scheduler
@@ -16,6 +51,8 @@ class FloydServiceActor extends Actor {
   // the HttpService trait defines only one abstract member, which
   // connects the services environment to the enclosing actor or test
   def actorRefFactory = context
+
+  val streamActor = context.actorOf(Props[StreamerActor], name="streamer-full")
 
   def receive = {
     // when a new  connection comes in we register ourselves as the connection handler
@@ -26,8 +63,7 @@ class FloydServiceActor extends Actor {
       sender ! index
 
     case HttpRequest(GET, Uri.Path("/stream"), _, _, _) =>
-      val peer = sender // since the Props creator is executed asyncly we need to save the sender ref
-      context actorOf Props(new Streamer(peer, 25))
+      streamActor ! StartStream(sender)
 
     case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
       println(Thread.currentThread)
@@ -62,41 +98,12 @@ class FloydServiceActor extends Actor {
     )
   )
 
-  class Streamer(client: ActorRef, count: Int) extends Actor with ActorLogging {
-    log.debug("Starting streaming response ...")
-
-    // we use the successful sending of a chunk as trigger for scheduling the next chunk
-    client ! ChunkedResponseStart(HttpResponse(entity = "")).withAck(Ok(count))
-
-    def receive = {
-      case Ok(0) =>
-        log.info("Finalizing response stream ...")
-        client ! MessageChunk("\nStopped...")
-        client ! ChunkedMessageEnd
-        context.stop(self)
-
-      case Ok(remaining) =>
-        log.info("Sending response chunk ...")
-        context.system.scheduler.scheduleOnce(1000 millis span) {
-          val json = s"""{
-              date:"${DateTime.now.toIsoDateTimeString.toString()}",
-              count:"$remaining"
-            }
-            """
-          client ! MessageChunk(json).withAck(Ok(remaining - 1))
-        }
-
-      case x: Http.ConnectionClosed =>
-        log.info("Canceling response stream due to {} ...", x)
-        context.stop(self)
-    }
-
-    // simple case class whose instances we use as send confirmation message for streaming chunks
-    case class Ok(remaining: Int)
-  }
+  
 
 
 }
 
 
+
+  case class StartStream(client: ActorRef)
 

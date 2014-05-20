4d446e5c956297605fc6dd44984c3898498c4077
Cruz Fernandez <cruz@scvsoft.com>
2014-05-20 13:04:22 -0300

    Moved logic of Streamer to a new file. Refactored to create an actor per request (of stream). Added update method with post logic and redistribute through the supervisor mechanism and actor selection

--- src/main/scala/io/floyd/FloydServiceActor.scala
+++ src/main/scala/io/floyd/FloydServiceActor.scala
@@ -1,6 +1,6 @@
 package io.floyd
 
-import akka.actor.{Actor,Props,ActorLogging, ActorRef}
+import akka.actor.{OneForOneStrategy, Identify, ActorLogging, Props}
 import spray.routing._
 import spray.http._
 import MediaTypes._
@@ -8,48 +8,11 @@ import spray.can.Http
 import HttpMethods._
 import MediaTypes._
 import scala.concurrent.duration._
-import scala.collection.mutable
+import akka.actor.SupervisorStrategy.Restart
 
-
-case class StartStream(client: ActorRef)
-
-// simple case class whose instances we use as send confirmation message for streaming chunks
-case class Ok(remaining: Int, client: ActorRef)
-
-class StreamerActor extends Actor with ActorLogging {
-  import context.dispatcher // ExecutionContext for the futures and scheduler
-
-  // we use the successful sending of a chunk as trigger for scheduling the next chunk
-
-  def receive = {
-    case StartStream(client) =>
-      client ! ChunkedResponseStart(HttpResponse(entity = "")).withAck(Ok(5, client))
-
-    case Ok(0, client) =>
-      log.info("Finalizing response stream ...")
-      client ! MessageChunk("\nStopped...")
-      client ! ChunkedMessageEnd
-
-    case Ok(remaining, client) =>
-      log.info("Sending response chunk ...")
-      log.info(this.toString())
-      log.info(self.toString())
-      context.system.scheduler.scheduleOnce(1000 millis span) {
-        val json = s"""{
-            date:"${DateTime.now.toIsoDateTimeString.toString()}",
-            count:"$remaining"
-          }
-          """
-        client ! MessageChunk(json).withAck(Ok(remaining - 1, client))
-      }
-  }
-
-}
-class FloydServiceActor extends HttpServiceActor {
+class FloydServiceActor extends HttpServiceActor with ActorLogging {
   
   import context.dispatcher // ExecutionContext for the futures and scheduler
-
-  val streamActor = context.actorOf(Props[StreamerActor], name="streamer-full")
 
   def receive = runRoute {
     path("ping") {
@@ -58,7 +21,19 @@ class FloydServiceActor extends HttpServiceActor {
       }
     } ~
     path("stream") { ctx =>
-      streamActor ! StartStream(ctx.responder)
+      context.actorOf(StreamerActor.props(ctx.responder)) ! StartStream()
+    } ~
+    (path("update") & post){
+      entity(as[String]) { data =>
+        complete {
+          log.info("children list " + context.children.toString())
+          context.actorSelection("*") ! Update(data)
+          "sent update to all streams"
+        }
+      }
+    } ~
+    path("part2.html") { ctx =>
+      context.actorOf(StreamerActor.props(ctx.responder)) ! StartStream()
     } ~
     path("stop") {
       complete {
@@ -89,6 +64,7 @@ class FloydServiceActor extends HttpServiceActor {
             <li><a href="/stream">/stream</a></li>
             <li><a href="/crash">/crash</a></li>
             <li><a href="/stop">/stop</a></li>
+            <li><a href="/update">post here /update</a></li>
           </ul>
         </body>
       </html>.toString()
---
+++ src/main/scala/io/floyd/StreamerActor.scala
@@ -0,0 +1,38 @@
+package io.floyd
+
+import akka.actor._
+import spray.http.{ChunkedResponseStart, HttpResponse, MessageChunk,
+  ChunkedMessageEnd, DateTime}
+import spray.can.Http
+import spray.can.Http.ConnectionClosed
+import spray.http.HttpResponse
+import spray.http.ChunkedResponseStart
+
+case class StartStream()
+
+// simple case class whose instances we use as send confirmation message for streaming chunks
+case class Ok(remaining: Int, client: ActorRef)
+
+case class Update(data: String)
+
+object StreamerActor {
+  def props(client: ActorRef): Props = Props(new StreamerActor(client))
+}
+
+class StreamerActor(client: ActorRef) extends Actor with ActorLogging {
+  def receive = {
+    case StartStream() =>
+      client ! ChunkedResponseStart(HttpResponse(entity = ""))
+
+    case Http.PeerClosed =>
+      log.info("killing " + self.toString() )
+      self ! PoisonPill
+
+    case Update(data) =>
+      log.info("parent " + context.parent.toString())
+      log.info("children list " + context.children.toString())
+      val json = s"""{ data:"${data}" }\n"""
+      client ! MessageChunk(json)
+  }
+
+}

8a26abaf4f6f2fcac5b929de4f3df2ff2fc69691
Cruz Fernandez <cruz@scvsoft.com>
2014-06-12 14:54:27 -0300

    Fixed code cleannes on test, as it's not necessary to implement a wait on an ask specifically

--- src/test/scala/TestAuthenticatorActor.scala
+++ src/test/scala/TestAuthenticatorActor.scala
@@ -1,38 +1,23 @@
 import akka.testkit.TestActorRef
 
-import akka.util.Timeout
-import akka.pattern.ask
-
 import io.floyd.{InvalidUser, ValidUser, User, AuthenticatorActor}
-
-import scala.concurrent._
-import scala.concurrent.duration._
 
 class TestAuthenticatorActor extends BaseUnitTestActor {
   val authenticator = TestActorRef[AuthenticatorActor]
 
-  implicit val timeout = Timeout(5 seconds)
-
-  def futureShouldBe(future: Future[Any], validUser: Boolean) = {
-    val resultFromFuture:Any = Await.result(future, timeout.duration)
-    if (validUser)
-      resultFromFuture should be (ValidUser)
-    else
-      resultFromFuture should be (InvalidUser)
-  }
-
   "AuthenticatorActor" should "validate correct user in DB" in {
-    val future: Future[Any] = authenticator ? User("testEmail@yahoo.com", "password")
-    futureShouldBe(future, true)
+    authenticator ! User("testEmail@yahoo.com", "password")
+    expectMsg(ValidUser)
   }
 
   "Authenticator" should "invalidate inexistant user in DB" in {
-    val future = authenticator ? User("invalidEmail@yahoo.com", "password")
-    futureShouldBe(future, false)
+    authenticator ! User("invalidEmail@yahoo.com", "password")
+    expectMsg(InvalidUser)
   }
 
   "Authenticator" should "invalidate incorrect password user in DB" in {
-    val future = authenticator ? User("testEmail@yahoo.com", "passwordIncorrect")
-    futureShouldBe(future, false)
+    authenticator ! User("testEmail@yahoo.com", "passwordIncorrect")
+    expectMsg(InvalidUser)
   }
+
 }

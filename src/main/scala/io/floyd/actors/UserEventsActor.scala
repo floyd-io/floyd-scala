74155e2f701c403919dcf1da340a8c49b149a2b0
Cruz Fernandez <cruz@scvsoft.com>
2014-06-30 10:41:29 -0300

    Fixed send to inexistant actor when no streamer have been created yet

--- src/main/scala/io/floyd/actors/UserEventsActor.scala
+++ src/main/scala/io/floyd/actors/UserEventsActor.scala
@@ -9,14 +9,15 @@ class UserEventsActor extends Actor {
   def receive = {
     case StartStreamForUser(user, client) =>
       context.child(user) match {
-        case Some(actor) =>
-          actor ! client
+        case Some(actor) => actor ! client
         case None =>
           val eventsActor = context.actorOf(Props[EventsActor], user)
           eventsActor ! client
       }
     case UpdateForUser(user, data) =>
-      context.actorSelection(user) ! Update(data)
+      context.child(user) foreach { actor =>
+        actor ! Update(data)
+      }
   }
 
 }
